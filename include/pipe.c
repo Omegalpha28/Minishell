7decfa0ec84b78ee08b6df0f4d2871e3a493d635
Ossan Msoili <ossanmsoili@yahoo.com>
2024-03-12 10:17:21 +0100

    [UPDATE] adding semicolon condition

--- include/pipe.c
+++ include/pipe.c
@@ -15,8 +15,52 @@ static char **inspect_input(char *input, char **env)
     return env;
 }
 
-char **pipe_loop(char **my_tab, char **env, char *input)
+static int verif_pipe(char *input)
 {
-    env = inspect_input(input, env);
+    for (int i = 0; input[i]; i++) {
+        if (input[i] == '|')
+            return 0;
+    }
+    return 1;
+}
+
+static int verif_semicolon(char *input)
+{
+    for (int i = 0; input[i]; i++) {
+        if (input[i] == ';')
+            return 0;
+    }
+    return 1;
+}
+
+static char **comma_loop(char *input, char **env, int have_semicolon)
+{
+    char *input_copy = my_strdup(input);
+    char *partieRestante;
+    char *resultat;
+
+    if (input_copy[0] == ';')
+        input_copy++;
+    if (have_semicolon == 0) {
+        partieRestante = strtok(input_copy, ";");
+        while (partieRestante != NULL) {
+            resultat = malloc(sizeof(char *) * my_strlen(partieRestante));
+            resultat = partieRestante;
+            modify_struct(my_str_to_word_array(resultat));
+            partieRestante = strtok(NULL, ";");
+            env = inspect_input(resultat, env);
+        }
+    }
     return env;
+}
+
+char **my_comma(char **my_tab, char **env, char *input)
+{
+    int have_semicolon = verif_semicolon(input);
+
+    if (have_semicolon == 1) {
+        modify_struct(my_tab);
+        return (inspect_input(input, env));
+    } else
+        return (comma_loop(input, env, have_semicolon));
 }
